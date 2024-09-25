47468959fb81bc1ffd558bcf7f02e8c5d3e2e477
Yash Shivhare <shivhareyash19@gmail.com>
2024-09-25 15:36:15 +0530

    bcrypt replaced by bcyptJs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 const cloudinary = require("../cloudinaryConfig");
 const { error, success } = require("../utils/responseWrapper");
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const { error, success } = require("../utils/responseWrapper");
 const cloudinary = require("../cloudinaryConfig");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const getUser = async (req, res) => {
   try {
