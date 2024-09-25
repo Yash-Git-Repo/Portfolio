cadc8439e93e57601ded0c7f9fb47b6baa948f65
Yash Shivhare <shivhareyash19@gmail.com>
2024-09-25 15:52:36 +0530

    backendurl changes

--- portfolio/src/utils/axiosClient.js
+++ portfolio/src/utils/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://portfolio-backend-d9k6.onrender.com/api",
   withCredentials: true,
 });
